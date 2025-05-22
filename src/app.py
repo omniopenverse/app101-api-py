5da4e088715d1a1b23db4e95e7e9847a20a989a6
airseneo <temfack2arsene@gmail.com>
2025-05-21 22:15:54 -0500

    improving swagger and adding health check in dockerfile

--- src/app.py
+++ src/app.py
@@ -14,17 +14,15 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SWAGGER'] = {'title': 'User API', 'uiversion': 3}
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 swagger = Swagger(app)
 metrics = PrometheusMetrics(app)
 logging.basicConfig(level=logging.INFO)
 
 from routes import *
-
-# @app.route('/myactuator/health')
-# def health():
-#     return {'status': 'UP'}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
